096980ba43115aa2b1fe637e3fdd7f0b0c0af6d5
GAURAV KUMAR <153047679+Gaurav1764@users.noreply.github.com>
2026-06-24 15:16:27 +0530

    Update API_URL to new backend endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [editId, setEditId] = useState(null);
 
  
-  const API_URL = "https://ems-frontend-dc69.onrender.com";
+  const API_URL = "https://ems-backend-cgyt.onrender.com";
 
   // FETCH EMPLOYEES
   const getEmployees = async () => {
